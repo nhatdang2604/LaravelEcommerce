1437208f5342362775a1e5c441fdfb631926f3dd
dangkl123 <nhatdang2604@gmail.com>
2022-12-12 10:56:13 +0700

    Migrate products and images table

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -25,11 +25,19 @@
                 <li class="nav-item"> <a class="nav-link" href="{{url('admin/category')}}">View Category</a></li>
               </ul>
             </div>
+          </li>
           <li class="nav-item">
-            <a class="nav-link" href="pages/charts/chartjs.html">
-              <i class="mdi mdi-chart-pie menu-icon"></i>
-              <span class="menu-title">Products</span>
+            <a class="nav-link" data-bs-toggle="collapse" href="#products" aria-expanded="false" aria-controls="products">
+              <i class="mdi mdi-plus-circle menu-icon"></i>
+              <span class="menu-title">Product</span>
+              <i class="menu-arrow"></i>
             </a>
+            <div class="collapse" id="products">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="{{url('admin/products/create')}}">Add Product</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{url('admin/products')}}">View Product</a></li>
+              </ul>
+            </div>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{url('admin/brands')}}">
