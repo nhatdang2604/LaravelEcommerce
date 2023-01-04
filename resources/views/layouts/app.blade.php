16132e1d0f6c945b6e7a6ddf01a9479ddc1523f5
dangkl123 <nhatdang2604@gmail.com>
2023-01-04 18:15:52 +0700

    Finish chap 17

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title> @yield('title') </title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -15,7 +15,10 @@
 
     <!-- Scripts -->
     <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
-    @vite(['public/assets/css/bootstrap.min.css'])
+    @vite([
+        'public/assets/css/bootstrap.min.css',
+        'public/assets/css/custom.css',
+    ])
 
     @livewireStyles
 </head>
