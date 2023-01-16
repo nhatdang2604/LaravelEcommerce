d0e008132a66bbe7f0855f3e9c90662b97c4613b
dangkl123 <nhatdang2604@gmail.com>
2023-01-16 17:29:50 +0700

    Finish chap 58

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,8 @@ Route::middleware(['auth'])->group(function() {
     Route::get('/thank-you', [App\Http\Controllers\Frontend\CheckoutController::class, 'thankYou']);
     Route::get('/orders', [App\Http\Controllers\Frontend\OrderController::class, 'index']);
     Route::get('/orders/{orderId}', [App\Http\Controllers\Frontend\OrderController::class, 'show']);
+    Route::get('/profile', [App\Http\Controllers\Frontend\UserController::class, 'index']);
+    Route::post('/profile', [App\Http\Controllers\Frontend\UserController::class, 'updateUserDetails']);
 
 });
 
