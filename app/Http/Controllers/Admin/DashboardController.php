5e8aec4e804af1a3a097dcaf905a50d78f53ece8
dangkl123 <nhatdang2604@gmail.com>
2023-01-16 15:57:27 +0700

    Finish chap 56

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Brand;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('admin.dashboard');
+
+        //Get some total number
+        $totalProducts = Product::count();
+        $totalCategories = Category::count();
+        $totalBrands = Brand::count();
+        $totalAllUsers = User::count();
+        $totalOrders = Order::count();
+        $totalUsers = User::where('role_as', '0')->count(); //user role count
+        $totalAdmins = User::where('role_as', '1')->count();    //admin role count
+
+        //Get order by today, this month and this year
+        $now = Carbon::now();
+        $today = $now->format('d-m-Y');
+        $thisMonth = $now->format('m');
+        $thisYear = $now->format('Y');
+        $todayOrders = Order::whereDate('created_at', $today)->count();
+        $thisMonthOrders = Order::whereMonth('created_at', $thisMonth)->count();
+        $thisYearOrders = Order::whereYear('created_at', $thisYear)->count();
+
+
+        return view('admin.dashboard', compact(
+            'totalProducts', 'totalCategories', 'totalBrands', 'totalAllUsers', 'totalOrders',
+            'totalUsers', 'totalAdmins', 'todayOrders', 'thisMonthOrders', 'thisYearOrders',
+        ));
     }
+
+
 }
