072f4d8eb7d9951a6ecdda08e93d3534b5a86745
dangkl123 <nhatdang2604@gmail.com>
2023-01-17 15:05:54 +0700

    Temporaly finish chap 61

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,7 +21,15 @@
     <!-- endinject -->
     <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
     <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
+    <style>
+        .form-control{
+            border: 1px solid #ddd;
+        }
 
+        .sidebar .nav .nav-item.active{
+            background-color: #e9e9e9;
+        }
+    </style>
     @livewireStyles
 </head>
 <body>
